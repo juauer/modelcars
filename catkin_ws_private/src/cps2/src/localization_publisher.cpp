d2ff4374bdc431041a6593039794a5fac8934bf0
juauer <julius.auer@gmail.com>
2017-05-26 13:21:28 +0200

    Add a localization publisher.

---
+++ catkin_ws_private/src/cps2/src/localization_publisher.cpp
@@ -0,0 +1,49 @@
+#include <ros/ros.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <nav_msgs/Odometry.h>
+#include <image_transport/image_transport.h>
+#include <tf/tf.h>
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/core.hpp>
+
+bool ready = false;
+
+// ? particleFilter;
+cv::Mat image;
+cv::Point3f pose;
+geometry_msgs::PoseStamped msg_pose;
+nav_msgs::Odometry msg_odo;
+ros::Publisher pub;
+
+void callback_odometry(const nav_msgs::Odometry &msg) {
+	msg_odo = msg;
+	ready   = true;
+}
+
+void callback_image(const sensor_msgs::ImageConstPtr &msg) {
+	if(!ready)
+		return;
+
+	image                     = cv_bridge::toCvShare(msg, "bgr8")->image;
+	// pose                      = particleFilter.getBest(image, msg_odo);
+	msg_pose.header.seq       = msg->header.seq;
+	msg_pose.header.stamp     = msg->header.stamp;
+	msg_pose.pose.position.x  = pose.x;
+	msg_pose.pose.position.y  = pose.y;
+	msg_pose.pose.orientation = tf::createQuaternionFromYaw(pose.z);
+	pub.publish(msg_pose);
+}
+
+int main(int argc, char **argv) {
+	ros::init(argc, argv, "localization_cps2_publisher");
+	ros::NodeHandle nh;
+	image_transport::ImageTransport it(nh);
+
+	ros::Subscriber sub_odo             = nh.subscribe("/odom", 1, &callback_odometry);
+	image_transport::Subscriber sub_img = it.subscribe("/usb_cam/image_undistorted", 1, &callback_image);
+
+	msg_pose.pose.position.z = 0;
+	pub                      = nh.advertise<geometry_msgs::PoseStamped>("/localization/cps2/pose", 1);
+
+	ros::spin();
+}
