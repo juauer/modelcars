a5dd732e661b69050ff94c59d370cbcb02a55a6c
juauer <julius.auer@gmail.com>
2017-05-26 12:29:27 +0200

    Add Hamid's generic PF.

---
+++ catkin_ws_private/src/cps2/src/hamid/Particle.h
@@ -0,0 +1,29 @@
+/*
+ * Particle.h
+ *
+ *  Created on: Dec 9, 2011
+ *      Author: hamid
+ */
+
+#ifndef PARTICLE_H_
+#define PARTICLE_H_
+
+class Particle {
+public:
+	Particle(){};
+	virtual ~Particle(){};
+
+	int ID;
+	double belief;
+
+	virtual void evaluate(void) = 0;
+
+	virtual void initializeRandom() = 0;
+
+	virtual void initializeAt() = 0;
+
+	virtual void getShitCoordinates(int &px, int &py) = 0;
+
+};
+
+#endif /* PARTICLE_H_ */
