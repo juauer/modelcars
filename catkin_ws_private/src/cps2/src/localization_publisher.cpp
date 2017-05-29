6880d65262960d1573d0aa62f98496f81300b81f
mauriceAlthoff <maurice@althoff.me>
2017-05-29 14:35:29 +0000

    merge conflict

--- catkin_ws_private/src/cps2/src/localization_publisher.cpp
+++ catkin_ws_private/src/cps2/src/localization_publisher.cpp
@@ -4,13 +4,14 @@
 #include <nav_msgs/Odometry.h>
 #include <opencv2/core.hpp>
 #include <ros/ros.h>
+#include <ros/package.h>
 #include <tf/tf.h>
 #include "particle3f.hpp"
 
 bool ready = false;
 
-cps2::Map map;
-cps2::ParticleFilter3f pf(map,1000);
+cps2::Map *map;
+cps2::ParticleFilter3f pf(*map,1000);
 
 cv::Mat image;
 cv::Point3f pose;
@@ -18,11 +19,11 @@ geometry_msgs::PoseStamped msg_pose;
 nav_msgs::Odometry msg_odo;
 ros::Publisher pub;
 
-Particle3f convOdomMsg2Particle3f(nav_msgs::Odometry msg) {
-  Particle3f p;
-  p.p.x = msg.pose.position.x;
-  p.p.y = msg.pose.position.y;
-  p.p.z = msg.pose.position.z;
+cps2::Particle3f convOdomMsg2Particle3f(nav_msgs::Odometry msg) {
+  cps2::Particle3f p(map);
+  p.p.x = msg.pose.pose.position.x;
+  p.p.y = msg.pose.pose.position.y;
+  p.p.z = msg.pose.pose.position.z;
 
   //msg.twist.twist.linear.x  = vx;
   //msg.twist.twist.linear.y  = vy;
@@ -30,7 +31,6 @@ Particle3f convOdomMsg2Particle3f(nav_msgs::Odometry msg) {
   
   return p;
 }
-
 
 void callback_odometry(const nav_msgs::Odometry &msg) {
   msg_odo = msg;
@@ -42,13 +42,13 @@ void callback_image(const sensor_msgs::ImageConstPtr &msg) {
     return;
 
   image = cv_bridge::toCvShare(msg, "bgr8")->image;
-  particleFilter.evaluate(image, convOdomMsg2Particle3f(msg_odo));
-  Particle3f pose  = particleFilter.getBest();
+  pf.eval(image, convOdomMsg2Particle3f(msg_odo));
+  cps2::Particle3f pose = pf.getBest();
   msg_pose.header.seq = msg->header.seq;
   msg_pose.header.stamp = msg->header.stamp;
-  msg_pose.pose.position.x = pose.x;
-  msg_pose.pose.position.y = pose.y;
-  msg_pose.pose.orientation = tf::createQuaternionFromYaw(pose.z);
+  msg_pose.pose.position.x = pose.p.x;
+  msg_pose.pose.position.y = pose.p.y;
+  //msg_pose.pose.orientation = tf::createQuaternionFromYaw(pose.p.z);
   pub.publish(msg_pose);
 }
 
@@ -57,6 +57,11 @@ int main(int argc, char **argv) {
   ros::NodeHandle nh;
   image_transport::ImageTransport it(nh);
 
+  std::string path = ros::package::getPath("cps2") +
+                     std::string("/../../../captures/map_test.jpg");
+
+  map = new cps2::Map(path.c_str());
+  
   ros::Subscriber sub_odo = nh.subscribe("/odom", 1, &callback_odometry);
   image_transport::Subscriber sub_img =
       it.subscribe("/usb_cam/image_undistorted", 1, &callback_image);
