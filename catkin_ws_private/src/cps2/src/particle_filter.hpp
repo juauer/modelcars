ba8ed52574c854fb3992337b816ffca759846ccb
juauer <julius.auer@gmail.com>
2017-06-09 16:34:14 +0200

    Überflüssige (redundante) Bestrafung entfernen.

--- catkin_ws_private/src/cps2/src/particle_filter.hpp
+++ catkin_ws_private/src/cps2/src/particle_filter.hpp
@@ -71,13 +71,6 @@ class ParticleFilter {
       it->p.x += dx;
       it->p.y += dy;
       it->p.z = it->p.z + dt;
-
-      // punish particles that are outside the map
-      if (it->p.x >= (map->img_gray.cols - 1) ||
-          it->p.y >= (map->img_gray.rows - 1) ||
-          it->p.x < 0|| it->p.y < 0){
-        it->belief *= punishEdgeParticlesRate;
-      }
     }
   }
 
