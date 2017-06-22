3b3983f177c19ffb94d7e5a66af1d946e08f9eef
maurice <maurice@althoff.me>
2017-06-22 15:06:28 +0000

    control first implementation

--- catkin_ws_private/src/control/src/control_node.cpp
+++ catkin_ws_private/src/control/src/control_node.cpp
@@ -1,91 +1,73 @@
-#include "ros/ros.h"
+#include <ros/ros.h>
 #include <std_msgs/Int16.h>
-#include <geometry_msgs/Twist.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/Point.h>
+#include <opencv2/core.hpp>
 
 //parameters
 #include <ros/ros.h>
 
 class Control {
  public:
-  Control(ros::NodeHandle nh) {
+  Control(ros::NodeHandle nh):seqNum(0) {
     n_.param<int>("mode", mode, 0);
-    pubSpeed_=nh.advertise<std_msgs::Int16>(nh.resolveName("manual_control/speed"), 1);
-    pubSteering_=nh.advertise<std_msgs::Int16>(nh.resolveName("manual_control/steering"), 1);
-    //subScan_ = n_.subscribe("scan", 1, &Control::scanCallback,this);
-    subTwist_ = n_.subscribe("motor_control/twist",1,&Control::speedCallback,this); 
+    n_.param<int>("dstPosX", dstPosX, 0);
+    n_.param<int>("dstPosY", dstPosY, 0);
+    n_.param<int>("speed", speed, 20);
+
+    dst.x = dstPosX;
+    dst.y = dstPosY;
+    dst.z = 0; // because we move on a 2d plane
+    
+    pubSteering_ = nh.advertise<std_msgs::Int16>(nh.resolveName("manual_control/steering"), 1);
+    pubDst_ = nh.advertise<geometry_msgs::Point>(nh.resolveName("/control/destination"), 1);
+    subDir_ = n_.subscribe("/localization/cps2/pose",1,&Control::setDirection,this); 
+    subDst_ = n_.subscribe("/localization/cps2/dst",1,&Control::setDestination,this); 
   }
   ~Control(){}
 
-  void speedCallback(const geometry_msgs::Twist& twist) {
-    direction=twist.linear.x;
+  void setDirection(const geometry_msgs::PoseStamped& msg_pose) {
+    if (mode ==0){
+    seqNum = msg_pose.header.seq;
+
+    cv::Point2f c_pos;
+    
+    c_pos.x = msg_pose.pose.position.x;
+    c_pos.x = msg_pose.pose.position.y;
+    
+    msg_pose.pose.orientation.x;
+    msg_pose.pose.orientation.y;
+    msg_pose.pose.orientation.z;
+    msg_pose.pose.orientation.w;
+    
+    //pubSpeed_.publish(speed_msg);
+    //pubSteering_.publish(steering_angle_msg);
+    }else{
+      // just set a const angle
+      steering_angle_msg.data = mode-91;
+      pubSteering_.publish(steering_angle_msg);
+    }
+  }
+  
+  void setDestination(const geometry_msgs::Point& msg_dst) {
+    dst.x = msg_dst.x;
+    dst.y = msg_dst.y;
   }
 
-  // void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan) {
-  //   int count = scan->scan_time / scan->time_increment;
-  //   float  break_distance_=break_distance;
-  //   ros::Time begin = ros::Time::now();
-  //   if (abs(direction)>500)
-  //     break_distance_=(abs(direction)/500)*break_distance;
-  //   //ROS_INFO("speed %f",break_distance_);	
-  //   if(direction < 0){	//backw.
-  //     for(int i = 0; i < (angle_back/2)+1; i++) {
-  //       if (scan->ranges[i] <= break_distance_) {
-  //         speed.data=5000;
-  //         pubSpeed_.publish(speed);
-  //         steering_angle.data=90-steering_angle_data;
-  //         speed.data=0;
-  //         pubSpeed_.publish(speed);
-  //         pubSteering_.publish(steering_angle);
-  //         ros::Duration(0.5).sleep();
-  //         speed.data=1000;
-  //         pubSpeed_.publish(speed);
-  //         //ROS_INFO("Obstacle");
-  //         return;
-  //       }
-  //     }
-  //     for(int k = (360-(angle_back/2)); k < count; k++) {
-  //       if (scan->ranges[k] <= break_distance_) {
-  //         speed.data=5000;
-  //         pubSpeed_.publish(speed);
-  //         steering_angle.data=90-steering_angle_data;
-  //         speed.data=0;
-  //         pubSpeed_.publish(speed);
-  //         pubSteering_.publish(steering_angle);
-  //         ros::Duration(0.5).sleep();
-  //         speed.data=1000;
-  //         pubSpeed_.publish(speed);
-  //         return;
-  //       }
-  //     }
-  //   }
-
-  //   if(direction > 0) { //forw.
-  //     for(int j = (180-(angle_front/2)); j < (180+(angle_front/2))+1; j++) {
-  //       if (scan->ranges[j] <= break_distance_) {
-  //         speed.data=-5000;
-  //         pubSpeed_.publish(speed);
-  //         steering_angle.data=90+steering_angle_data;
-  //         speed.data=0;
-  //         pubSpeed_.publish(speed);
-  //         pubSteering_.publish(steering_angle);
-  //         ros::Duration(0.5).sleep();
-  //         speed.data=-1000;
-  //         pubSpeed_.publish(speed);
-  //         return;
-  //       }
-  //     }
-  //   }
-  // }
-
- private:
-  std_msgs::Int16 steering_angle;
-  std_msgs::Int16 speed;
-  int direction;
+  int seqNum;
   int mode;
+  int dstPosX;
+  int dstPosY;
+  int speed;
+ protected:
+  geometry_msgs::Point dst;
+  std_msgs::Int16 speed_msg;
+  std_msgs::Int16 steering_angle_msg;
   ros::NodeHandle n_; 
-  ros::Publisher pubSpeed_;
   ros::Publisher pubSteering_;
-  ros::Subscriber subTwist_;
+  ros::Publisher pubDst_;
+  ros::Subscriber subDir_;
+  ros::Subscriber subDst_;
 
 };//End of class auto_stop
 
@@ -93,7 +75,10 @@ int main(int argc, char **argv) {
   ros::init(argc, argv, "control_node");
   ros::NodeHandle nh; 
   Control control(nh);
-
+  
+  ROS_INFO("Control_node: mode: %d desired:(%d,%d) speed: %d",
+           control.mode, control.dstPosX, control.dstPosY, control.speed);
+  
   while(ros::ok()) {
     ros::spin();
   }
