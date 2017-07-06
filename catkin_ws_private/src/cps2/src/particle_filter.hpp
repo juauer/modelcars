903ef53d331f7ba93b7019d46ba80af842c31366
juauer <julius.auer@gmail.com>
2017-07-06 16:40:35 +0200

    Apply code style.

--- catkin_ws_private/src/cps2/src/particle_filter.hpp
+++ catkin_ws_private/src/cps2/src/particle_filter.hpp
@@ -10,36 +10,12 @@
 namespace cps2 {
 
 class ParticleFilter {
- public:
-  cps2::Map *map;
-  cps2::ImageEvaluator *image_evaluator;
-
-  const int particles_num;
-  const int particles_keep;
-  const float particle_belief_scale;
-  const float particle_stdev_lin;
-  const float particle_stdev_ang;
-  const float punishEdgeParticlesRate;
-  const bool hamid_sampling;
-  const bool binning_enabled;
-  const float bin_size;
-  bool setStartPos;
-  const cv::Point3f startPos;
-  
-  std::vector<Particle> particles;
-  Particle best_single;
-  Particle best_binning;
-  std::random_device rd;  //Will be used to obtain a seed for the random number engine
-  std::mt19937 gen; //Standard mersenne_twister_engine
-  std::uniform_real_distribution<float> udist_x;
-  std::uniform_real_distribution<float> udist_y;
-  std::uniform_real_distribution<float> udist_t;
+public:
 
   ParticleFilter(cps2::Map *_map, cps2::ImageEvaluator *_image_evaluator, int _particles_num,
-                 float _particles_keep, float _particle_belief_scale,
-                 float _particle_stdev_lin, float _particle_stdev_ang, 
-                 bool _hamid_sampling, float _bin_size, float _punishEdgeParticlesRate,
-                 bool _setStartPos, cv::Point3f _startPos);
+                 float _particles_keep, float _particle_belief_scale, float _particle_stdev_lin,
+                 float _particle_stdev_ang, bool _hamid_sampling, float _bin_size,
+                 float _punishEdgeParticlesRate, bool _setStartPos, cv::Point3f _startPos);
 
   ~ParticleFilter();
 
@@ -54,6 +30,30 @@ class ParticleFilter {
   Particle getBest();
 
   void binning();
+
+  const int particles_num;
+  const int particles_keep;
+  const float particle_belief_scale;
+  const float particle_stdev_lin;
+  const float particle_stdev_ang;
+  const float punishEdgeParticlesRate;
+  const bool hamid_sampling;
+  const bool binning_enabled;
+  const float bin_size;
+  const cv::Point3f startPos;
+  
+  cps2::Map *map;
+  cps2::ImageEvaluator *image_evaluator;
+
+  bool setStartPos;
+  std::vector<Particle> particles;
+  Particle best_single;
+  Particle best_binning;
+  std::random_device rd;  //Will be used to obtain a seed for the random number engine
+  std::mt19937 gen; //Standard mersenne_twister_engine
+  std::uniform_real_distribution<float> udist_x;
+  std::uniform_real_distribution<float> udist_y;
+  std::uniform_real_distribution<float> udist_t;
 };
 
 } // namespace cps2
